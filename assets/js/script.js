25a60e4299d198b23db78fafac2faaf879065e04
Melina Vidoni <melina.vidoni@rmit.edu.au>
2021-08-19 08:05:10 +1000

    Changing colour scheme

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,15 +2,6 @@ function updateStyle() {
   // Get the profile
   var profile = document.getElementById("profile");
 
-  // Create the new element
-  var rmit = document.createElement("img");
-  rmit.setAttribute("border", "0");
-  rmit.setAttribute("src", "/img/rmitlogo.png");
-  rmit.setAttribute("width", "30%");
-  rmit.classList.add("logoimg");
-
-  // Append
-  profile.appendChild(rmit);
 
 
 
